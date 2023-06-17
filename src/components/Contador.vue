e175ea24e8553e09f65051c8fbb44f2370adc60d
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-17 10:51:47 -0500

    Taller 18: optionAPI props, compute e importacion de un componente

---
+++ src/components/Contador.vue
@@ -0,0 +1,60 @@
+<template>
+  <h3>{{ tituloComponente }}</h3>
+  <h3>{{ titulo }}</h3>
+  <p>{{ numero }}<sup>2 </sup>={{ obtenerCuadradoComputado }}</p>
+  <p>{{ numero }}<sup>2 </sup>={{ obtenerCuadradoComputado }}</p>
+  <p>{{ numero }}<sup>2 </sup>={{ obtenerCuadradoComputado }}</p>
+  <button v-on:click="incrementar()">+</button>
+  <button v-on:click="decrementar()">-</button>
+</template>
+
+<script>
+export default {
+  //dar un nombre especifico al componente, no siempre va a ser igual al del arvhibo
+  name: "Contador",
+  data() {
+    return {
+      numero: 5,
+      tituloComponente: this.titulo,
+    };
+  },
+  methods: {
+    obtenerCuadrado() {
+      console.log("Entro al metodo cuadrado");
+      return this.numero * this.numero;
+    },
+
+    incrementar() {
+      this.numero += 1;
+    },
+
+    decrementar() {
+      this.numero -= 1;
+    },
+  },
+
+  computed: {
+    obtenerCuadradoComputado() {
+      console.log("Entro al metodo cuadrado computado");
+      return this.numero * this.numero;
+    },
+  },
+  props: ["titulo", "valor2"],
+};
+</script>
+
+<style>
+button {
+  background-color: #64b687;
+  border-radius: 5px;
+  border: 1px solid white;
+  color: white;
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px 15px;
+}
+
+button:hover {
+  background-color: #03c457;
+}
+</style>
